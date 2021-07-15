5f0cd42b59879d6fb28d3f21936fd37e60e1297b
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2021-07-15 09:51:44 -0400

    refactor: idetetion and words consert

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,24 +1,24 @@
-import { Request, Response, NexFunction } from 'express'
+import { Request, Response, NextFunction } from 'express'
 import * as jwt from 'jsonwebtoken'
 
 
 //midleware, só poderá acessar as rotas se o token for enviado no cabeçalho
-export const auth = async (req: Request, res: Response, next: NexFunction)=>{
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
 
     const authHeader = req.headers.authorization
-    
-    if(!authHeader){
-        return res.status(401).json({message: 'Token is required!'})
+
+    if (!authHeader) {
+        return res.status(401).json({ message: 'Token is required!' })
     }
 
     //Emana o Bearer e o token, o split devolve o token
     const [, token] = authHeader.split(' ')
 
-    try{
+    try {
 
         await jwt.verify(token, process.env.APP_SECRET)
         next()
-    }catch(error){
-        return res.status(401).json({message: 'Token invalid or expired!'})
+    } catch (error) {
+        return res.status(401).json({ message: 'Token invalid or expired!' })
     }
 }
