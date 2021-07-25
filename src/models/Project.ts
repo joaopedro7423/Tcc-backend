f653f23661a767cc2bfac5f66fdc4f2c87f54173
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2021-07-25 02:49:40 -0400

    feat: add ProjectModels

---
+++ src/models/Project.ts
@@ -0,0 +1,44 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import ProjectStatus from "../enums/projectStatus";
+import User from "./Users";
+
+@Entity("projects")
+export default class Project {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: "user_id" })
+  user: string;
+
+  @Column({
+    type: "varchar",
+  })
+  status: ProjectStatus;
+
+  @Column()
+  logo: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  create_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
