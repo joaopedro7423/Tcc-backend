6d865345b7fba1091b340d6385673bc8eb998d3e
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2021-07-25 02:48:20 -0400

    feat: add ProjectsDTOS

---
+++ src/dtos/ICreateProjectDTO.ts
@@ -0,0 +1,9 @@
+import ProjectStatus from "../enums/projectStatus";
+
+export default interface ICreateProjectDTO {
+  name: string;
+  user_id: string;
+  description: string;
+  logo?: string;
+  status: ProjectStatus;
+}
