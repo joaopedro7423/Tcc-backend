7b52fe761ab2b06b3220fa709bcc528220870e7f
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2021-08-10 21:14:42 -0400

    feat: add Student into project

---
+++ src/database/migrations/1628557247607-AddUserStudentInProject.ts
@@ -0,0 +1,31 @@
+import {MigrationInterface, QueryRunner, Table, TableColumn, TableForeignKey} from "typeorm";
+
+export class AddUserStudentInProject1628557247607 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.addColumn(
+        "projects",
+        new TableColumn({
+          name: "student_id",
+          type: "uuid",
+          isNullable: true,
+        })
+      );
+
+
+      await queryRunner.createForeignKey('projects', new TableForeignKey({
+        name: 'ProjectStudentFk',
+        columnNames: ['student_id'],
+        referencedColumnNames:['id'],
+        referencedTableName:'student',
+        onDelete:'SET NULL',
+        onUpdate: 'CASCADE'
+      }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropColumn('projects','student_id')
+      await queryRunner.dropForeignKey('projects','ProjectStudentFk' )
+    }
+
+}
