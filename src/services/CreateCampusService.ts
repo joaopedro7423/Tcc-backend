472dfbf1fe9e09b44ba130f532e0f23744e52ed3
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2021-08-11 23:30:18 -0400

    feat: add campus Create Service

---
+++ src/services/CreateCampusService.ts
@@ -0,0 +1,31 @@
+import Campus from '../models/Campus';
+import AppError from '../errors/AppError';
+
+import ICampusRepository from '../repositories/ICampusRepository';
+import CampusRepository from '../repositories/CampusRepository';
+
+interface Request {
+  name: string;
+}
+
+//essa parada (Service) aqui que se faz as regras de negócio
+export default class CreateCampusService {
+  private campusRepository: ICampusRepository;
+
+  constructor(campusRepository: CampusRepository) {
+    this.campusRepository = campusRepository;
+  }
+  public async execute({ name }: Request): Promise<Campus> {
+
+    const campusExist = await this.campusRepository.findOneByName(name);
+
+    if (campusExist) {
+      //console.log(campusExist)
+      throw new AppError('Campus já utilizado', 401);
+    }
+
+    const campus = await this.campusRepository.create(name);
+
+    return campus;
+  }
+}
