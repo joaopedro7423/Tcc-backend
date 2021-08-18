a07d62ad97f668881680f4bdd5d135856cf94c38
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2021-08-17 21:28:30 -0400

    feat: create proposals controller

---
+++ src/controller/ProposalsController.ts
@@ -0,0 +1,110 @@
+import { Request, Response } from "express";
+
+import ProjectsRepository from "../repositories/ProjectsRepository";
+import UsersRepository from "../repositories/UsersRepository";
+
+import CreateProjectService from "../services/CreateProjectService";
+import ShowProjectService from "../services/ShowProjectService";
+import ListAllProjectsService from "../services/ListAllProjectsService";
+import UpdateProjectService from "../services/UpdateProjectService";
+import UpdateProjectStatusService from "../services/UpdateProjectStatusService";
+import UploadLogoOfProjectProjectService from "../services/UploadLogoOfProjectProjectService";
+
+export default class ProposalsController {
+  //para achar todos os users listar claro
+  public async index(req: Request, res: Response): Promise<Response> {
+    const projectsRepository = new ProjectsRepository();
+
+    const projectsService = new ListAllProjectsService(projectsRepository);
+
+    const projects = await projectsService.execute();
+
+    return res.json(projects);
+  }
+
+  public async show(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const projectsRepository = new ProjectsRepository();
+
+    const projectsService = new ShowProjectService(projectsRepository);
+
+    const project = await projectsService.execute(id);
+
+    return res.json(project);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, user_id, description, student_id } = request.body;
+    const projectsRepository = new ProjectsRepository();
+    const userRepository = new UsersRepository();
+    const createProject = new CreateProjectService(
+      projectsRepository,
+      userRepository
+    );
+
+    const project = await createProject.execute({
+      name,
+      student_id,
+      user_id,
+      description,
+      logo: request.file?.filename,
+    });
+
+    return response.status(201).json(project);
+  }
+
+  public async uploadLogo(
+    request: Request,
+    response: Response
+  ): Promise<Response> {
+    const { id } = request.params;
+    const { filename } = request.file;
+    const projectsRepository = new ProjectsRepository();
+    const service = new UploadLogoOfProjectProjectService(projectsRepository);
+
+    const project = await service.execute({
+      id,
+      logo: filename,
+    });
+
+    return response.json(project);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const { name, description, user_id } = request.body;
+    const projectsRepository = new ProjectsRepository();
+    const userRepository = new UsersRepository();
+    const updateProject = new UpdateProjectService(
+      projectsRepository,
+      userRepository
+    );
+
+    const project = await updateProject.execute({
+      id,
+      name,
+      user_id,
+      description,
+    });
+
+    return response.json(project);
+  }
+
+  public async chengeStatus(
+    request: Request,
+    response: Response
+  ): Promise<Response> {
+    const { id } = request.params;
+    const { status } = request.body;
+    const projectsRepository = new ProjectsRepository();
+
+    const updateProject = new UpdateProjectStatusService(projectsRepository);
+
+    const project = await updateProject.execute({
+      id,
+      status,
+    });
+
+    return response.json(project);
+  }
+}
