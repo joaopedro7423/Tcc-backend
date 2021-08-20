66dc03becb38a04effcc6e9f5d9f6ecc069a3b72
Joao Pedro <joaopedro7423@hotmail.com>
2021-08-20 09:15:41 -0300

    rename: rename table migration name student to notifications

--- src/database/migrations/1628557026370-CreateNotification.ts
+++ src/database/migrations/1628557026370-CreateNotification.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateStudants1628557026370 implements MigrationInterface {
+export default class CreateNotifications1628557026370 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
