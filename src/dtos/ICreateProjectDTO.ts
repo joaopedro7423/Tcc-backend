bd32a7c3f17ea861cf1a31b11516c76c36bbac92
Joao Pedro <joaopedro7423@hotmail.com>
2021-08-24 16:31:34 -0300

    refactor: chenge project dto variables

--- src/dtos/ICreateProjectDTO.ts
+++ src/dtos/ICreateProjectDTO.ts
@@ -1,9 +1,5 @@
-import ProjectStatus from "../enums/projectStatus";
 
 export default interface ICreateProjectDTO {
-  name: string;
-  user_id: string;
-  description: string;
-  logo?: string;
-  status?: ProjectStatus;
+ title: string
+ description: string
 }
