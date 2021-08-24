1e0fef5c84322a007910f4ae604a55cffa0a7037
Joao Pedro <joaopedro7423@hotmail.com>
2021-08-24 16:42:23 -0300

    refactor: update variables in update project service

--- src/services/UpdateProjectService.ts
+++ src/services/UpdateProjectService.ts
@@ -1,54 +1,37 @@
-import AppError from "../errors/AppError";
+import AppError from '../errors/AppError';
 
-import IProjectsRepository from "../repositories/IProjectsRepository";
-import Project from "../models/Project";
-import ProjectStatus from "../enums/projectStatus";
-import IUsersRepository from "../repositories/IUsersRepository";
+import IProjectsRepository from '../repositories/IProjectsRepository';
+import Project from '../models/Project';
+import ProjectStatus from '../enums/projectStatus';
 
 interface IRequest {
   id: string;
-  name: string;
-  logo?: string;
+  title: string;
   description: string;
-  user_id: string;
 }
 
 //essa parada (Service) aqui que se faz as regras de negócio
 export default class UpdateProjectService {
   private projectRepository: IProjectsRepository;
 
-  private userRepository: IUsersRepository;
-
-  constructor(
-    projectRepository: IProjectsRepository,
-    userRepository: IUsersRepository
-  ) {
+  constructor(projectRepository: IProjectsRepository) {
     this.projectRepository = projectRepository;
-    this.userRepository = userRepository;
   }
   public async execute({
     id,
-    name,
-    user_id,
+    title,
+
     description,
-    logo,
   }: IRequest): Promise<Project> {
     const project = await this.projectRepository.findById(id);
 
     if (!project) {
-      throw new AppError("Project not found!", 400);
+      throw new AppError('Project not found!', 400);
     }
 
-    const userExist = await this.userRepository.findById(user_id);
+    project.title = title;
 
-    if (!userExist) {
-      throw new AppError("User not found!", 400);
-    }
-
-    project.name = name;
-    project.user_id = user_id;
     project.description = description;
-    project.logo = logo;
 
     await this.projectRepository.save(project);
 
