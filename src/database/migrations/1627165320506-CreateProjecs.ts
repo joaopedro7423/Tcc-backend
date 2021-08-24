d8688811b3dc3a5024324d8d05db6d0f40f9acb8
Joao Pedro <joaopedro7423@hotmail.com>
2021-08-24 16:30:00 -0300

    refactor: refactor migration project

--- src/database/migrations/1627165320506-CreateProjecs.ts
+++ src/database/migrations/1627165320506-CreateProjecs.ts
@@ -1,65 +1,69 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export default class CreateProjecs1627165320506 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: "projects",
+        name: 'projects',
         columns: [
           {
-            name: "id",
-            type: "uuid",
+            name: 'id',
+            type: 'uuid',
             isPrimary: true,
-            generationStrategy: "uuid",
-            default: "uuid_generate_v4()",
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
-            name: "name",
-            type: "varchar",
+            name: 'title',
+            type: 'varchar',
           },
           {
-            name: "user_id",
-            type: "uuid",
+            name: 'description',
+            type: 'text',
           },
           {
-            name: "status",
-            type: "varchar",
+            name: 'user_create_id',
+            type: 'uuid',
           },
           {
-            name: "logo",
-            type: "varchar",
+            name: 'user_accept_id',
+            type: 'uuid',
             isNullable: true,
           },
           {
-            name: "description",
-            type: "text",
+            name: 'create_at',
+            type: 'timestamp',
+            default: 'now()',
           },
           {
-            name: "create_at",
-            type: "timestamp",
-            default: "now()",
-          },
-          {
-            name: "updated_at",
-            type: "timestamp",
-            default: "now()",
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
           },
         ],
         foreignKeys: [
           {
-            name: "ProjectsUser",
-            columnNames: ["user_id"],
-            referencedColumnNames: ["id"],
-            referencedTableName: "users",
-            onDelete: "SET NULL",
-            onUpdate: "CASCADE",
+            name: 'ProjectUserCreate',
+            columnNames: ['user_create_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'users',
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+          {
+            name: 'ProjectUserAccept',
+            columnNames: ['user_accept_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'users',
+            onDelete: 'SET NULL',
+            onUpdate: 'CASCADE',
           },
         ],
-      })
+      }),
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable("projects");
+    await queryRunner.dropTable('projects');
   }
 }
