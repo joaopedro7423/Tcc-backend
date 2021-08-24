f2913207f37ebe47e8d21f4d8506a01c9d0b6f81
Joao Pedro <joaopedro7423@hotmail.com>
2021-08-24 16:34:45 -0300

    feat: add create project on update proposals

--- src/services/UpdateProposalStatusService.ts
+++ src/services/UpdateProposalStatusService.ts
@@ -2,6 +2,7 @@ import AppError from '../errors/AppError';
 
 import IProposalsRepository from '../repositories/IProposalsRepository';
 import Proposal from '../models/Proposals';
+import ProjectsRepository from '../repositories/ProjectsRepository';
 
 interface IRequest {
   id: string;
@@ -10,20 +11,38 @@ interface IRequest {
 
 //essa parada (Service) aqui que se faz as regras de negócio
 export default class UpdateProposalStatusService {
-  private proposalRepository: IProposalsRepository;
 
-  constructor(proposalRepository: IProposalsRepository) {
+  private proposalRepository: IProposalsRepository;
+  private projectRepository: ProjectsRepository;
+
+  constructor(proposalRepository: IProposalsRepository, projectRepository:ProjectsRepository) {
     this.proposalRepository = proposalRepository;
+    this.projectRepository = projectRepository;
   }
   public async execute({ id, user_accept_id }: IRequest): Promise<Proposal> {
     const proposal = await this.proposalRepository.findById(id);
+
+  
 
     if (!proposal) {
       throw new AppError('Proposal not found!', 400);
     }
 
+    
+
     proposal.user_accept_id = user_accept_id;
 
+    await this.proposalRepository.save(proposal);
+
+
+    const project =  await this.projectRepository.create({
+    title : proposal.title,
+    description: proposal.description
+    })
+
+    this.projectRepository.save(project)
+
+    proposal.project_id = project.id;
     await this.proposalRepository.save(proposal);
 
     return proposal;
