bddc2bce35e5171395dc4fdd0dd3ede1b5279af8
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2021-08-26 22:33:23 -0400

    feat: create activities IRepository

---
+++ src/repositories/IActivitiesRepository.ts
@@ -0,0 +1,10 @@
+import CreateActivitiesDTO from'../dtos/ICreateActivitiesDTO';
+import Activities from '../models/Activities';
+
+export default interface IActivitiesRepository {
+  findById(id: string): Promise<Activities | undefined>;
+  findAll(): Promise<Activities[]>;
+  create(createActivitiesDTO: CreateActivitiesDTO): Promise<Activities>;
+  save(course: Activities): Promise<Activities>;
+  delete(id: string): Promise<void>;
+}
